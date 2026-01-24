617971bd193d0ca66508822e58db36603f1df332
Nelson-Arenas <racacaca_@hotmail.com>
2026-01-24 20:33:25 -0300

    Se agrega Imagen Emin

--- src/views/registerView.tsx
+++ src/views/registerView.tsx
@@ -4,6 +4,9 @@ export default function RegisterView() {
     return (
         <> 
             <div>Register View</div>
+            <div>
+                <img src="/grupo-emin.jpg" alt="Logotipo Emin" />
+            </div>
 
             <nav>
                 <Link to="/auth/login">Volver al login</Link>
